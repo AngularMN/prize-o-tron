6139963d9046ff69ef94dc752ff39d428cd5e0b2
Dale 'Ducky' Lotts <dlotts@knightrider.com>
2017-03-04 23:15:21 -0600

    test(e2e): Fix failing e2e unit tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('prize-o-tron App', () => {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Prize-O-Tron');
   });
 });
